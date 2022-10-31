75ed1b284e250bbe4fe7c15b8986e96f25f691d1
Chigozirim <iamokwukwe@gmail.com>
2022-10-31 14:06:01 +0100

    Create DBClient class to manage MongoDB client connection.

---
+++ utils/db.js
@@ -0,0 +1,29 @@
+import { MongoClient, Server } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || '27017';
+    const database = process.env.DB_DATABASE || 'files_manager';
+
+    MongoClient.connect(new Server(host, port)).then((client) => {
+      this.db = client.db(database);
+      this.usersCollection = this.db.collection('users');
+      this.filesCollection = this.db.collection('files');
+    });
+  }
+
+  isAlive() {
+    return !!this.db;
+  }
+
+  async nbUsers() {
+    return this.usersCollection.countDocuments({});
+  }
+
+  async nbFiles() {
+    return this.filesCollection.countDocuments({});
+  }
+}
+
+module.exports = new DBClient();
