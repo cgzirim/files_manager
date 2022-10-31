5e469f4e9fb4eabbf84182de01fcfac87ce0607e
Chigozirim <iamokwukwe@gmail.com>
2022-10-31 13:14:18 +0100

    Create RedisClient class to manage Redis client connection

---
+++ utils/redis.js
@@ -0,0 +1,30 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+        this.client = createClient();
+        this.get = promisify(this.client.get).bind(this.client);
+        this.client.on('error', (err) => console.log(err.message));
+    }
+
+    isAlive() {
+       return this.client.connected;
+    }
+
+    async get(key) {
+        const value = await this.get(key);
+        return value
+    }
+
+    async set(key, value, duration) {
+        this.client.setex(key, duration, value);
+    }
+
+    async del(key) {
+        this.client.del(key);
+    }
+}
+
+
+module.exports = new RedisClient()
