6ff4036131b631db82d2c9bb0cc32219b5a2a4e9
Chigozirim <iamokwukwe@gmail.com>
2022-11-05 04:07:49 +0100

    Fix Error: request entity too large

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ import express from 'express';
 import router from './routes/index';
 
 const app = express();
+
 const port = process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
 app.use('/', router);
 app.listen(port);
 
