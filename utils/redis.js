478e3e7ba5752ff9e731c8223d4f9d965d7efc77
Chigozirim <iamokwukwe@gmail.com>
2022-11-12 01:04:30 +0100

    Write unittest for redis client.

--- utils/redis.js
+++ utils/redis.js
@@ -3,27 +3,28 @@ import { promisify } from 'util';
 
 class RedisClient {
   constructor() {
-        this.client = createClient();
-        this.get = promisify(this.client.get).bind(this.client);
-        this.client.on('error', (err) => console.log(err.message));
-    }
+    this.client = createClient();
+    this.get = promisify(this.client.get).bind(this.client);
+    this.client.on('error', (err) => console.log(err.message));
+  }
 
-    isAlive() {
-       return this.client.connected;
-    }
+  isAlive() {
+    return this.client.connected;
+  }
 
-    async get(key) {
-        const value = await this.get(key);
-        return value;
-    }
+  async get(key) {
+    const value = await this.get(key);
+    return value;
+  }
 
-    async set(key, value, duration) {
-        this.client.setex(key, duration, value);
-    }
+  async set(key, value, duration) {
+    this.client.setex(key, duration, value);
+  }
 
-    async del(key) {
-        this.client.del(key);
-    }
+  async del(key) {
+    this.client.del(key);
+  }
 }
 
-module.exports = new RedisClient();
+const redisClient = new RedisClient();
+module.exports = redisClient;
