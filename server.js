5e3356ff097775af0102ec51c51da2df9cdfb50a
Chigozirim <iamokwukwe@gmail.com>
2022-12-03 01:09:32 +0100

    Enable Express to parse multipart/form-data

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 import express from 'express';
+import multer from 'multer';
+import swaggerUI from 'swagger-ui-express';
+import docs from './docs/index';
 import router from './routes/index';
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 
+// Parse application/json
 app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true }));
+
+// Parse multipart/form-data
+app.use(multer().single('data'));
+app.use(express.static('public'));
+
 app.use('/', router);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(docs));
 app.listen(port);
 
 module.exports = app;
